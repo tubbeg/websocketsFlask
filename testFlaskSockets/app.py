2bf64009c2517b98f5159247a92874875d8f1b01
Tobias Gärdebro <tubbeg@gmail.com>
2020-05-30 11:14:54 +0200

    First commit. Websockets are working with socketio.

---
+++ testFlaskSockets/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+@socketio.on('messageEvent')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+if __name__ == '__main__':
+    socketio.run(app)
